8696d0e6623d1c0452c5a1496a3a981d09665f57
mattpedone <mattpedone7@gmail.com>
2021-05-06 15:54:04 +0200

    script sistemato & commentato

--- R/mixture_mcmc.R
+++ R/mixture_mcmc.R
@@ -1,21 +1,70 @@
 rm(list=ls())
 source(file = "R/utils.R")
+set.seed(121)
+y <- gendata()
+##uncommenting the following line and setting G = 5, nu = 150, alphag = 1.5 we can observe label switching
+#y <- scale(MASS::galaxies/1000)
 
-data <- gendata()
-Ndraw <- 1000
-mix=list(k=3,mu=mean(data),sig=var(data))
-simu=my_gibbs_s(Ndraw, dat = data, mix = mix)
-hist(data,prob=TRUE,main="",xlab="",ylab="",nclass=100)
-x=y=seq(min(data),max(data),length=150)
-yy=matrix(0,ncol=150,nrow=Ndraw)
-for (i in 1:150){
-  yy[,i]=apply(simu$p*dnorm(x[i],mean=simu$mu,
-                            sd=sqrt(simu$sig)),1,sum)
-  y[i]=mean(yy[,i])
+#quantities for MCMC & hyperparameter specification
+ndraw <- 10000
+G <- 3
+mixpar <- list(G = G, mu = mean(y), sig = var(y), nu = 20, alphag = .5)#nu = 150, alphag = 1.5#
+
+#run Gibbs sampler
+out <- my_gibbs_s(ndraw, y = y, mixpar = mixpar)
+
+#mixture density plot
+hist(y, prob=TRUE, main="", xlab="", ylab="", nclass=100, ylim = c(0, .5))#1.5
+hist(y, prob=TRUE, main="", xlab="", ylab="", nclass=100, ylim = c(0, 0.5))#2.0
+x <- yl <- seq(min(y), max(y), length=150)
+yy <- matrix(0, ncol=150, nrow=ndraw)
+
+for(i in 1:150){
+  yy[,i]=apply(out$p*dnorm(x[i],mean=out$mu, sd=sqrt(out$sig)),1,sum)
+  yl[i]=mean(yy[,i], na.rm = T)
 }
-for (t in (Ndraw/2):Ndraw)
-  lines(x,yy[t,],col="gold")
-lines(x,y,lwd=2.3,col="red")
 
-simu$k
-apply(simu$mu, 2, summary)
+for(t in (ndraw/2):ndraw){
+  lines(x, yy[t,], col="gold")
+}
+
+lines(x, yl, lwd=2.3,col="red")
+
+#Convergence of the three types of parameters of the normal mixture
+colors <- c("steelblue3", "sienna3", "gold4", "azure3", "aquamarine2")
+plot(out$p[,1],type="l", col="steelblue3", xlab="iterations",ylab="p", ylim = c(0, 0.6))
+for(g in 2:G){
+  lines(out$p[,g],type="l",col=colors[g],xlab="iterations",ylab="p")
+}
+
+plot(out$mu[,1],type="l", col="steelblue3", xlab="iterations",ylab=expression(mu), 
+     ylim = c(-2.5, 2.5))
+for(g in 2:G){
+  lines(out$mu[,g],type="l",col=colors[g],xlab="iterations",ylab=expression(mu))
+}
+
+plot(out$sig[,1],type="l", col="steelblue3", xlab="iterations",ylab=expression(sigma), 
+     ylim = c(0.0, 0.5))
+for(g in 2:G){
+  lines(out$sig[,g],type="l",col=colors[g],xlab="iterations",ylab=expression(sigma))
+}
+
+#2 x 2 plot of the Gibbs sample for the three types of parameters of a normal mixture
+plot(out$mu[,1], out$p[,1], col="steelblue3", xlim = c(-2.5, 2.5), ylim = c(0, 0.6), 
+     xlab=expression(mu), ylab="p")
+for(g in 2:G){
+  points(out$mu[,g], out$p[,g], col=colors[g])
+}
+
+plot(out$sig[,1], out$p[,1], col="steelblue3", xlim = c(0.0, 0.8), ylim = c(0.0, 0.6), 
+     xlab=expression(sigma), ylab="p")
+for(g in 2:G){
+  points(out$sig[,g], out$p[,g], col=colors[g])
+}
+
+plot(out$mu[,1], out$sig[,1], col="steelblue3", xlim = c(-2.5, 2.5), ylim = c(0, .75), 
+     xlab=expression(mu), ylab=expression(sigma))
+for(g in 2:G){
+  points(out$mu[,g], out$sig[,g], col=colors[g])
+}
+
--- R/utils.R
+++ R/utils.R
@@ -35,52 +35,70 @@ rdirichlet <- function(n = 1, alpha = c(1,1,1)){
   return(q)
 }
 
-my_gibbs_s <- function (niter, dat, mix) 
-{
-  n = length(dat)
-  k = mix$k
-  z = rep(0, n)
-  ssiz = nxj = ssum = rep(0, k)
-  mug = sigg = prog = matrix(0, nrow = niter, ncol = k)
-  lopost = rep(0, niter)
-  lik = matrix(0, n, k)
-  prog[1, ] = rep(1, k)/k
-  mug[1, ] = rep(mix$mu, k)
-  sigg[1, ] = rep(mix$sig, k)
-  for (j in 1:k) lik[, j] = prog[1, j] * dnorm(x = dat, mean = mug[1, 
-                                                                   j], sd = sqrt(sigg[1, j]))
-  lopost[1] = sum(log(apply(lik, 1, sum))) + sum(dnorm(mug[1, 
-  ], mean(dat), sqrt(sigg[1, ]), log = TRUE)) - (10 + 1) * 
-    sum(log(sigg[1, ])) - sum(var(dat)/sigg[1, ]) + 0.5 * 
-    sum(log(prog[1, ]))
-  for (i in 1:(niter - 1)) {
-    for (t in 1:n) {
-      prob = prog[i, ] * dnorm(dat[t], mean = mug[i, ], 
-                               sd = sqrt(sigg[i, ]))
-      #cat("iter: ", i, ", prob = ", prob, ", sum: ", sum(prob), "\n")
-      if ((sum(prob) == 0))  
-        #if ((sum(prob) == 0) | (is.nan(sum(prob))))  
-        prob = rep(1, k)/k
-      z[t] = sample(1:k, 1, prob = prob)
-    }
-    for (j in 1:k) {
-      ssiz[j] = sum(z == j)
-      nxj[j] = sum(as.numeric(z == j) * dat)
-    }
-    mug[i + 1, ] = rnorm(k, (mean(dat) + nxj)/(ssiz + 1), 
-                         sqrt(sigg[i, ]/(ssiz + 1)))
-    for (j in 1:k) ssum[j] = sum(as.numeric(z == j) * (dat - 
-                                                         nxj[j]/ssiz[j])^2)
-    sigg[i + 1, ] = 1/rgamma(k, shape = 0.5 * (20 + ssiz), 
-                             rate = var(dat) + 0.5 * ssum + 0.5 * ssiz/(ssiz + 
-                                                                          1) * (mean(dat) - nxj/ssiz)^2)
-    prog[i + 1, ] = rdirichlet(1, par = ssiz + 0.5)
-    for (j in 1:k) lik[, j] = prog[i + 1, j] * dnorm(x = dat, 
-                                                     mean = mug[i + 1, j], sd = sqrt(sigg[i + 1, j]))
-    lopost[i + 1] = sum(log(apply(lik, 1, sum))) + sum(dnorm(mug[i + 
-                                                                   1, ], mean(dat), sqrt(sigg[i + 1, ]), log = TRUE)) - 
-      (10 + 1) * sum(log(sigg[i + 1, ])) - sum(var(dat)/sigg[i + 
-                                                               1, ]) + 0.5 * sum(log(prog[i + 1, ]))
+my_gibbs_s <- function (niter, y, mixpar){
+  #quantities & hyperparameters
+  n <- length(y)#number of observations
+  G <- mixpar$G #number of components
+  nu <- mixpar$nu#prior for shaper parameter IG
+  alphag <- mixpar$alpha#prior for Dirichlet
+  mg <- mean(y)#prior mean for m_g
+  s2 <- var(y)#rate parameter for IG
+  
+  #objects to store stuff
+  z <- rep(0, n)#latent variables
+  ng <- ngyz <- ssum <- rep(0, G)#n_g, n_g\times\bar{y}_g(\bm z), n_g\times\hat{s}_{g}^{2}
+  mug <- sigg <- etag <- matrix(0, nrow = niter, ncol = G)#\mu_g, \sigma_g, eta_g
+  logpost <- rep(0, niter)#logposterior
+  lik <- matrix(0, n, G)#likelihood
+  
+  #initialization (riga 1 algorithm 1)
+  etag[1, ] <- rep(1, G)/G
+  mug[1, ] <- rep(mixpar$mu, G)
+  sigg[1, ] <- rep(mixpar$sig, G)
+  for(g in 1:G){
+    lik[, g] <- etag[1, g] * dnorm(x = y, mean = mug[1, g], sd = sqrt(sigg[1, g]))
   }
-  list(k = k, mu = mug, sig = sigg, p = prog, lopost = lopost)
+  logpost[1] <- sum(log(apply(lik, 1, sum))) + sum(dnorm(mug[1,], mg, sqrt(sigg[1, ]), log = TRUE)) - 
+    (10 + 1) * sum(log(sigg[1, ])) - sum(c(s2)/sigg[1, ]) + 0.5 * sum(log(etag[1, ]))
+  
+  #begins the MCMC (riga 2 algorithm 1)
+  for(t in 1:(niter - 1)){
+    #loop over observation (riga 3 algorithm 1)
+    for(i in 1:n){
+      #compute p(z_i=g|\theta, \eta) eq. 2.1
+      prob <- etag[t, ] * dnorm(y[i], mean = mug[t, ], sd = sqrt(sigg[t, ]))
+      #for numerical stability
+      prob[which(is.nan(prob))] <- 10.0^(-4)
+      if((sum(prob) == 0)){
+        prob <- rep(1, G)/G
+      }
+      z[i] <- sample(1:G, 1, prob = prob)
+    }
+    
+    #compute useful quantities
+    for(g in 1:G){
+      ng[g] <- sum(z == g)#n_g
+      ngyz[g] <- sum(as.numeric(z == g) * y)#n_g\times\bar{y}_g(\bm z)
+    }
+    
+    #posterior sampling
+    mug[t + 1, ] <- rnorm(G, (mg + ngyz)/(ng + 1), sqrt(sigg[t, ]/(ng + 1)))
+    for(g in 1:G){
+      ssum[g] = sum(as.numeric(z == g) * (y - ngyz[g]/ng[g])^2)#n_g\times\hat{s}_{g}^{2}
+      }
+    sigg[t + 1, ] = 1/rgamma(G, shape = 0.5 * (nu + ng), rate = c(s2) + 0.5 * ssum + 0.5 * ng/(ng + 1) * (mg - ngyz/ng)^2)
+    etag[t + 1, ] = rdirichlet(1, alpha = ng + alphag)
+    
+    #update quantities (likelihood & loglikelihood)
+    for(g in 1:G){
+      lik[, g] = etag[t + 1, g] * dnorm(x = y, mean = mug[t + 1, g], sd = sqrt(sigg[t + 1, g]))
+      }
+    logpost[t + 1] = sum(log(apply(lik, 1, sum))) + 
+      sum(dnorm(mug[t + 1, ], mg, sqrt(sigg[t + 1, ]), log = TRUE)) - 
+      (10 + 1) * sum(log(sigg[t + 1, ])) - sum(c(s2)/sigg[t + 1, ]) + 
+      0.5 * sum(log(etag[t + 1, ]))
+  }
+  #return
+  res <- list(G = G, mu = mug, sig = sigg, p = etag, logpost = logpost)
+  return(res)
 }
